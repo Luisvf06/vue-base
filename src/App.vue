fd933ffc10939f6a6bcc378272fc9a59688548f6
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-02-15 09:19:48 +0000

    hacer el buscador

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import {ref} from 'vue'
-import Buscador from "./components/Buscador.vue";
-
-</script>
-
 <template>
   <div class="container">
     <header class="row align-items-center">
@@ -16,39 +9,49 @@ import Buscador from "./components/Buscador.vue";
         height="125"
       />
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" ><RouterLink to="/">Home</RouterLink></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page"><RouterLink to="/buscador">Buscador</RouterLink></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page"><RouterLink to="/watchlist">watchlist</RouterLink></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page"><RouterLink to="/film">Película</RouterLink></a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="Buscador">
-        <button class="btn btn-outline-success" type="submit" @click="$router.push('/api?cadena='+Buscador)">Search</button>
-
-      </form>
-    </div>
-  </div>
-</nav>
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" ><RouterLink to="/">Home</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page"><RouterLink to="/buscador">Buscador</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page"><RouterLink to="/watchlist">watchlist</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page"><RouterLink to="/film">Película</RouterLink></a>
+              </li>
+            </ul>
+            <form @submit.prevent="realizarBusqueda" class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="textoBusqueda">
+              <button class="btn btn-outline-success"@click="$router.push('buscador?caedna='+textoBusqueda)" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
       
     </header>
     <main>
       <RouterView />
+      <Buscador /> <!-- Utiliza el componente Buscador como una etiqueta -->
     </main>
   </div>
 </template>
 
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { ref } from 'vue';
+
+const textoBusqueda = ref('');
+
+const realizarBusqueda = () => {
+  // Puedes realizar acciones relacionadas con la búsqueda aquí
+  console.log('Realizando búsqueda:', textoBusqueda.value);
+}
+</script>
 
 <style scoped>
 header {
--- src/components/Buscador.vue
+++ src/components/Buscador.vue
@@ -1,12 +1,56 @@
 <template>
-
-  <p>{{ route.query.cadena }}</p>
+  <div class="row">
+    <div v-for="pelicula in peliculas" :key="pelicula.id" class="col-md-4 mb-4" @click="elegirPelicula(pelicula)">
+      <div class="card">
+        <img :src="getImagenPelicula(pelicula.poster_path)" class="card-img-top" alt="Movie Poster">
+        <span v-if="pelicula.vote_average.toFixed(2) >= 8.55" class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+          {{ pelicula.vote_average.toFixed(2) }}
+        </span>
+        <span v-else-if="pelicula.vote_average.toFixed(2) >= 7.0" class="position-absolute top-0 start-100 translate-middle p-2 bg-warning border border-light rounded-circle">
+          {{ pelicula.vote_average.toFixed(2) }}
+        </span>
+        <span v-else class="position-absolute top-0 start-100 translate-middle p-2 bg-info border border-light rounded-circle">
+          {{ pelicula.vote_average.toFixed(2) }}
+        </span>
+        <div class="card-body">
+          <h5 class="card-title">{{ pelicula.title }}</h5>
+          <span>{{ pelicula.overview.substring(0, 200) + '...' }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import {useRoute}  from 'vue-router';
-const route=useRoute();
+import { ref, onMounted } from 'vue';
 
+const apiKey = ref('4431fed8390b02d6c28655feb536156a');
+const bearerToken = ref('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDMxZmVkODM5MGIwMmQ2YzI4NjU1ZmViNTM2MTU2YSIsInN1YiI6IjY1YThmOTNlYzRmNTUyMDEyNzhlNjU2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nArKWLxihtW5aycNC-GAqUwF7JGeo_Rj13o_5ZA7K3w');
+const peliculas = ref([]);
 
+const obtenerPeliculasDesdeAPI = () => {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + apiKey.value)
+        .then(response => response.json())
+        .then(data => peliculas.value = data.results.slice(0, 3))
+}
+
+const obtenerPeliculasConHeaderAPI = () => {
+    fetch('https://api.themoviedb.org/3/movie/popular', {
+        headers: {
+            'Authorization': 'Bearer ' + bearerToken.value
+        }
+    })
+        .then(response => response.json())
+        .then(data => peliculas.value = data.results)
+}
+
+onMounted(() => {
+  obtenerPeliculasDesdeAPI()
+})
+
+const obtenerImagenPelicula = (rutaPoster) => {
+    if (rutaPoster) {
+        return 'https://image.tmdb.org/t/p/w500/' + rutaPoster;
+    }
+}
 </script>
